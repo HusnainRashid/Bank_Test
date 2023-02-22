b80501738a6fc0c89f2bda30e2a1a71ab2fbbfa2
your name <your email>
2023-02-22 12:14:44 +0000

    final commit. Not perfect but working

--- lib/techTest_1.rb
+++ lib/techTest_1.rb
@@ -1,27 +1,34 @@
 class BankAccount
   def initialize
     @currentBalance = 0
-    @records = "
-    date || credit || debit || balance"
+    @records = ""
   end
 
-  def deposit(cash)
+  def deposit(cash, date)
     @currentBalance += cash
+    full_record = "#{date} || #{cash} || || #{@currentBalance}
+"
+    @records << full_record
     return @currentBalance
   end
 
-  def withdraw(cash)
+  def withdraw(cash, date)
     @currentBalance -= cash
+    full_record = "#{date} || || #{cash} || #{@currentBalance}
+"
+    @records << full_record
     return @currentBalance
   end
 
-  def date(date, month, year)
-    time = Time.new
-    time.strftime("#{date}/%d/%Y")
-  end
-
-  def statement
-    puts @records
-    return options()
+  def statement()
+    return "date || credit || debit || balance
+" + @records
   end
 end
+
+myAccount = BankAccount.new
+myAccount.withdraw(500, "14/01/2023")
+myAccount.deposit(2000, "13/01/2023")
+myAccount.deposit(1000, "10/01/2023")
+
+puts myAccount.statement
--- spec/techTest_spec.rb
+++ spec/techTest_spec.rb
@@ -1,22 +1,56 @@
 require_relative "../lib/techTest_1"
 
 RSpec.describe "Test the deposit, withdrawal and statement of a bank account" do
+  it "returns an empty statement" do
+    myAccount = BankAccount.new
+    expect(myAccount.statement()).to eq "date || credit || debit || balance
+"
+  end
+
   it "returns a deposit of £2000" do
     myAccount = BankAccount.new
-    expect(myAccount.deposit(2000)).to eq 2000
+    expect(myAccount.deposit(2000, 21/10/1212)).to eq 2000
   end
 
   it "returns a withdrawal of £1000" do
     myAccount = BankAccount.new
-    expect(myAccount.withdraw(1000)).to eq -1000
+    expect(myAccount.withdraw(1000, 21/10/1212)).to eq -1000
   end
 
+  it "returns a withdraw statement" do
+    myAccount = BankAccount.new
+    myAccount.withdraw(500, "14/01/2023")
+    expect(myAccount.statement()).to eq "date || credit || debit || balance
+14/01/2023 || || 500 || -500
+"
+  end
+
+  it "returns a deposit statement" do
+    myAccount = BankAccount.new
+    myAccount.deposit(1000, "20/01/2023")
+    expect(myAccount.statement()).to eq "date || credit || debit || balance
+20/01/2023 || 1000 || || 1000
+"
+  end
+
+  it "returns multiple withdraw statements" do
+    myAccount = BankAccount.new
+    myAccount.withdraw(500, "14/01/2023")
+    myAccount.withdraw(500, "15/01/2023")
+    expect(myAccount.statement()).to eq "date || credit || debit || balance
+14/01/2023 || || 500 || -500
+15/01/2023 || || 500 || -1000
+"
+  end
   it "returns a full statement" do
     myAccount = BankAccount.new
-    myAccount.statement().to eq
-    date || credit || debit || balance
-    14/01/2023 || || 500.00 || 2500.00
-    13/01/2023 || 2000.00 || || 3000.00
-    10/01/2023 || 1000.00 || || 1000.00
+    myAccount.deposit(1000, "10/01/2023")
+    myAccount.deposit(2000, "13/01/2023")
+    myAccount.withdraw(500, "14/01/2023")
+    expect(myAccount.statement()).to eq "date || credit || debit || balance
+10/01/2023 || 1000 || || 1000
+13/01/2023 || 2000 || || 3000
+14/01/2023 || || 500 || 2500
+"
   end
 end
